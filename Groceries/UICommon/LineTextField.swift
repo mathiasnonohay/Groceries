c808af6889840b1c034770f1a5593b0cd7cb0f63
Mathias Nonohay <mathias.gianotti.nonohay@sibs.com>
2023-10-23 19:11:07 +0100

    Login screen layout

--- Groceries/UICommon/LineTextField.swift
+++ Groceries/UICommon/LineTextField.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 struct LineTextField: View {
+    
+    @Binding var text: String
+    @State var title: String = "Title"
+    @State var placeholder: String = "Placeholder"
+    @State var keyboardType: UIKeyboardType = .default
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(title)
+                .font(.customfont(.semibold, fontSize: 16))
+                .foregroundColor(.textTitle)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            
+
+            TextField(placeholder, text: $text)
+                .keyboardType(keyboardType)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .frame(height: 40)
+            
+            Divider()
+        }
     }
 }
 
-#Preview {
-    LineTextField()
+struct LineSecureField: View {
+    
+    @Binding var text: String
+    @Binding var isShowPassword: Bool
+    @State var title: String = "Title"
+    @State var placeholder: String = "Placeholder"
+
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.customfont(.semibold, fontSize: 16))
+                .foregroundColor(.textTitle)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            if isShowPassword {
+                TextField(placeholder, text: $text)
+                    .modifier(ShowButton(isShow: $isShowPassword))
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .frame(height:40)
+                
+            } else {
+                SecureField(placeholder, text: $text)
+                    .modifier(ShowButton(isShow: $isShowPassword))
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .frame(height:40)
+            }
+            
+            Divider()
+        }
+    }
+}
+
+struct LineTextField_Previews: PreviewProvider {
+    @State static var text: String = ""
+    
+    static var previews: some View {
+        LineTextField(text: $text)
+            .padding(20)
+    }
 }
--- Groceries/View/Login/LoginView.swift
+++ Groceries/View/Login/LoginView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @StateObject var loginViewModel = MainViewModel.shared
+//    @ObservedObject var loginViewModel = MainViewModel.shared;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("bottom_bg")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .screenWidth, height: .screenHeight)
+            
+            VStack {
+                Image("color_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .padding(.bottom, .screenWidth * 0.1)
+                
+                Text("Loging")
+                    .font(.customfont(.semibold, fontSize: 26))
+                    .foregroundColor(.primaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 4)
+                
+                Text("Enter your emails and password")
+                    .font(.customfont(.semibold, fontSize: 16))
+                    .foregroundColor(.secondaryText)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, .screenWidth * 0.1)
+                
+                LineTextField(text: $loginViewModel.txtEmail, title: "Email", placeholder: "Enter your email address", keyboardType: .emailAddress)
+                    .padding(.bottom, .screenWidth * 0.07)
+                
+                LineSecureField(text: $loginViewModel.txtPassword, isShowPassword: $loginViewModel.isShowPassword, title: "Password", placeholder: "Enter your password")
+                    .padding(.bottom, .screenWidth * 0.02)
+
+                Button {
+                    
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.customfont(.medium, fontSize: 14))
+                        .foregroundColor(.primaryText)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+                .padding(.bottom, .screenWidth * 0.02)
+                
+                RoundButton(title: "login") {
+                    
+                }
+                .padding(.bottom, .screenWidth * 0.07)
+                
+                HStack {
+                    Text("Don't have an acccount?")
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.primaryText)
+                    
+                    Text("Signup")
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.primaryApp)
+                }
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets + 64)
+            .padding(.horizontal, 20)
+            .padding(.bottom, .bottomInsets)
+            
+            VStack {
+                HStack {
+                    Button {
+                        mode.wrappedValue.dismiss()
+                    } label: {
+                        Image("back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets)
+            .padding(.horizontal, 20)
+        }
+        .background(Color.white)
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea()
     }
 }
 
