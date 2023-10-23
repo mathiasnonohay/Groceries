248c6a0ccf2d57f2fe33af2d9561726a0ade8c21
Mathias Nonohay <mathias.gianotti.nonohay@sibs.com>
2023-10-23 15:41:58 +0100

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by Mathias Nonohay on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
